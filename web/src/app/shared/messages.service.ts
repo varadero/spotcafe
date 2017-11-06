3ada8f54be12075125139460a44bef26e3c9cf14
Georgi Ganchev <admin@computerclubsystem.com>
2017-11-06 22:15:56 +0200

    SnackBar

--- web/src/app/shared/messages.service.ts
+++ web/src/app/shared/messages.service.ts
@@ -1,9 +1,18 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+
 import { IMessage } from './message';
 
+@Injectable()
 export class MessagesService {
     messages: IMessage[] = [];
 
+    constructor(private snackBarSvc: MatSnackBar) {
+    }
+
     addMessage(message: IMessage) {
         this.messages.push(message);
+        const additionalClass = 'alert-' + message.type;
+        this.snackBarSvc.open(message.text, undefined, { duration: 10000, extraClasses: [additionalClass] });
     }
 }
